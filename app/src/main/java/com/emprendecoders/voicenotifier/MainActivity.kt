42fa521634703f13665871cdd6da9c988ec97779
Tomas Mendoza <albertomendozainf@gmail.com>
2024-12-13 07:11:11 +0100

    add crashlytics

--- app/src/main/java/com/emprendecoders/voicenotifier/MainActivity.kt
+++ app/src/main/java/com/emprendecoders/voicenotifier/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.material3.Button
 import androidx.compose.material3.Snackbar
 import androidx.compose.runtime.mutableStateOf
 import androidx.core.app.ActivityCompat
